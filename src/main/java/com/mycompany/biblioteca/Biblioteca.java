03bef4afec4993c9cf555d565380bde2c7bb0685
Guilherme <guilhermeluiz.franco.dev@gmail.com>
2026-05-02 12:08:28 -0300

    Sexto commit - Separação de impressão no terminal (System.out.println) das funções e focando apenas no main.

--- src/main/java/com/mycompany/biblioteca/Biblioteca.java
+++ src/main/java/com/mycompany/biblioteca/Biblioteca.java
@@ -13,10 +13,10 @@ import java.util.*;
 public class Biblioteca {
 
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         Services service = new Services();
         boolean validacao = true;
-        boolean loopBreak = false;
         
         while (validacao){
             System.out.println("-----Bem vindo!-----");
@@ -27,120 +27,186 @@ public class Biblioteca {
             System.out.println("\n4- Sair");
             int op = sc.nextInt();
             
-            switch (op){
-                case 1:
-                    
-                 boolean loopBreak1 = false;
-                 boolean validacao1 = true;
-                 while(validacao1){
+        switch (op){
+            
+            case 1:
+                boolean validacaoLivro = true;
+                
+                while(validacaoLivro){
                   System.out.println("\n1- Cadastrar livro");
                   System.out.println("\n2- Listar livros");
                   System.out.println("\n3- Buscar livros");
                   System.out.println("\n4- Voltar");
-                  int op1 = sc.nextInt();  
+                  int opLivro = sc.nextInt(); 
+                  sc.nextLine();
                   
-                  if(op1 == 1){
+                  switch (opLivro){
                       
-                      try{
-                          service.CadastrarLivro();
-                      }catch(NumberFormatException e){
-                          System.out.println("Insira um número válido");
-                      }
-                    
-                 }else if(op1 == 2){
+                      case 1:
+                          
+                          try{
+                              
+                          System.out.println("Insira o código (ID) do livro: ");
+                          String idString = sc.nextLine();
+                          
+                          int id = Integer.parseInt(idString);
+                          
+                          System.out.println("\nInsira o titulo do livro: ");
+                          String Titulo = sc.nextLine();
+                          
+                          System.out.println("\nInsira o genero do livro: ");
+                          String Genero = sc.nextLine();
+                          
+                          System.out.println("\nInsira o autor do livro: ");
+                          String Autor = sc.nextLine();
+                          
+                          System.out.println("\nInsira o editor(a) do livro: ");
+                          String Editora = sc.nextLine();
+                          
+                          System.out.println("\nInsira o ano de publicação do livro: ");
+                          String AnoString = sc.nextLine();
+                          
+                          int Ano = Integer.parseInt(AnoString);
+                          
+                         System.out.println(service.CadastrarLivro(id, Titulo, Genero, Autor, Editora, Ano));
+                              
+                          }catch(NumberFormatException e){
+                           
+                           System.out.println("Por favor, insira um valor válida");
+                           
+                          }
+                          
+                          break;
+                          
+                      case 2:
+                          
+                         System.out.println("Escolha o filtro: ");
+                         System.out.println("\n| Titulo | Genero | Autor | Editora | Ano |");
+                         String opListar = sc.nextLine().toLowerCase();
+                         
+                         switch(opListar){ 
+                             
+                             case "titulo":
+                                 
+                               System.out.println("Insira o titulo: ");
+                               String valorTitulo = sc.nextLine();
+
+                                if(service.ListarLivros(opListar, valorTitulo).isEmpty()){
+                                    System.out.println("Nenhum livro encontrado");
+                                }else{
+                                    System.out.println(service.ListarLivros(opListar, valorTitulo)); 
+                                }
+                                
+                                break;
+                                
+                             case "genero":
+                                 
+                               System.out.println("Insira o genero: ");
+                               String valorGenero = sc.nextLine();
+
+                                if(service.ListarLivros(opListar, valorGenero).isEmpty()){
+                                    System.out.println("Nenhum livro encontrado");
+                                }else{
+                                    System.out.println(service.ListarLivros(opListar, valorGenero)); 
+                                }
+                                
+                                break;
+                             
+                             case "autor":
+                                 
+                               System.out.println("Insira o autor: ");
+                               String valorAutor = sc.nextLine();
+
+                                if(service.ListarLivros(opListar, valorAutor).isEmpty()){
+                                    System.out.println("Nenhum livro encontrado");
+                                }else{
+                                    System.out.println(service.ListarLivros(opListar, valorAutor)); 
+                                }
+                                
+                                break;
+                                
+                             case "editora":
+                                 
+                               System.out.println("Insira o editor(a): ");
+                               String valorEditora = sc.nextLine();
+
+                                if(service.ListarLivros(opListar, valorEditora).isEmpty()){
+                                    System.out.println("Nenhum livro encontrado");
+                                }else{
+                                    System.out.println(service.ListarLivros(opListar, valorEditora)); 
+                                }
+                                
+                                break;
+                            
+                             case "ano":
+                                 
+                               System.out.println("Insira o ano: ");
+                               String valorAno = sc.nextLine();
+
+                                if(service.ListarLivros(opListar, valorAno).isEmpty()){
+                                    System.out.println("Nenhum livro encontrado");
+                                }else{
+                                    System.out.println(service.ListarLivros(opListar, valorAno)); 
+                                }
+                                
+                                break;
+                                 
+                                
+                             default:
+                                 
+                                 System.out.println("Por favor, insira uma opção válida");
+                                 break;
+                         }
+                         
+                         break;
                      
-                     try{
-                         service.ListarLivros();
-                     }catch(NumberFormatException e){
-                         System.out.println("Insira um número válido");
-                     }
+                      case 3:
+                          
+                          System.out.println("Digite o código do livro: ");
+                          int Codigo = sc.nextInt();
+                          sc.nextLine();
+                             
+                          if(service.BuscarLivros(Codigo).isEmpty()){
+                              
+                            System.out.println("Nenhum livro encontrado");
+                          
+                          }else{
+                              
+                             System.out.println(service.BuscarLivros(Codigo)); 
+                                
+                          }
+                          
+                          break;
+                          
+                      case 4:
+                          validacaoLivro = false;
+                          
+                          break;
+                          
+                      default:
+                          System.out.println("Insira uma opção válida");
+                          break;
                     
-                 }else if(op1 == 3){
-                     
-                     try{
-                         service.BuscarLivros();
-                     }catch(NumberFormatException e){
-                         System.out.println("Insira um valor válido");
-                     }
+                  }
+                  
                     
-                 }else if (op1 == 4){
-                     loopBreak1 = true;
-                     break;
-                 }else{
-                     System.out.println("Por favor, insira um opção válida");
-                 }
-                  if(loopBreak) break;
-                 }
-                 break;
-                 
-                case 2:
-                    
-                 boolean loopBreak2 = false;
-                 boolean validacao2 = true;
-                 while(validacao2){
-                 System.out.println("\n1- Cadastrar usuarios");
-                 System.out.println("\n2- Listar usuarios");
-                 System.out.println("\n3- Buscar usuarios");
-                 System.out.println("\n4- Voltar");
-                 int op2 = sc.nextInt(); 
-                 
-                 if(op2 == 1){
-                    //função cadastrar usuarios 
-                 }else if(op2 == 2){
-                    //função listar usuarios 
-                 }else if(op == 3){
-                    //função buscar usuarios 
-                 }else if (op2 == 4){
-                     loopBreak2 = true;
-                     break;
-                 }else{
-                     System.out.println("Por favor, insira um opção válida");
-                 }
-                  if(loopBreak) break;
-                 } 
-                 
-                 break;
-                   
-                case 3:
-                    
-                 boolean loopBreak3 = false;
-                 boolean validacao3 = true;
-                 while(validacao3){
-                 System.out.println("\n1- Emprestar livro");
-                 System.out.println("\n2- Devolver livro");
-                 System.out.println("\n3- Voltar");
-                 int op3 = sc.nextInt();  
-                 
-                 
-                 if(op3 == 1){
-                    //função emprestar
-                 }else if(op3 == 2){
-                    //função devolver 
-                 }else if (op3 == 3){
-                     loopBreak3 = true;
-                     break;
-                 }else{
-                     System.out.println("Por favor, insira um opção válida");
-                 }
-                  if(loopBreak) break;
-                 } 
-                 
-                 break;
-                 
-                case 4: 
-                    
-                    loopBreak = true;
-                    break;
+                }
+                break;
+            case 2:
+                System.out.println("Funcionalidade em andamento");
+                break;
                 
-                default:
-                    System.out.println("Por favor, insira um opção válida");
-            }   
-            
-            if(loopBreak) break;
-            
-        
-        
-        }
+            case 3:
+                System.out.println("Funcionalidade em andamento");
+                break;
+            case 4:
+                validacao = false;
+                break;
+                
+        }  
         
     }
+    }      
 }
+    
+
--- src/main/java/com/mycompany/biblioteca/Services.java
+++ src/main/java/com/mycompany/biblioteca/Services.java
@@ -15,238 +15,123 @@ public class Services {
     
     Scanner sc = new Scanner(System.in);
     
-    public void CadastrarLivro(){
+    public Model.Livros CadastrarLivro(int ID, String Titulo, String Genero, String Autor, String Editora, int Ano){
            
-        
-           System.out.println("Insira o código (ID) do livro: ");
-           String idString = sc.nextLine();
-           int id = Integer.parseInt(idString);
-
-           System.out.println("\nInsira o titulo do livro: ");
-           String Titulo = sc.nextLine();
-           
-           System.out.println("\nInsira o genero do livro: ");
-           String Genero = sc.nextLine();
-           
-           System.out.println("\nInsira o autor do livro: ");
-           String Autor = sc.nextLine();
-           
-           System.out.println("\nInsira o editor(a) do livro: ");
-           String Editora = sc.nextLine();
-           
-           System.out.println("\nInsira o ano de publicação do livro: ");
-           String AnoString = sc.nextLine();
-       
-           int Ano = Integer.parseInt(AnoString);
-           
-           Model.Livros Livro = new Model().new Livros(id, Titulo, Genero, Autor, Editora, Ano);
+           Model.Livros Livro = new Model().new Livros(ID, Titulo, Genero, Autor, Editora, Ano);
            instancia.salvarLivro(Livro);
            
-           
-            System.out.println(Livro);
-           
-        }
+           return Livro;
       
+        }
     
-    public void ListarLivros(){
-        boolean controle = false;
-        System.out.println("Escolha o filtro: ");
-        System.out.println("\n| Titulo | Genero | Autor | Editora | Ano |");
-        String escolha = sc.nextLine();
+    public ArrayList ListarLivros(String escolha, String input){
+        Model.Livros livro;
+        ArrayList <Model.Livros> listaImpressão = new ArrayList<>();
         
-        String escolhaMinuscula = escolha.toLowerCase();
-        
-        switch (escolhaMinuscula){
-            
+        switch (escolha){
             case "titulo":
+             
+             for(int i = 0; i < instancia.listaLivro.size(); i++){
+             String valor = instancia.listaLivro.get(i).getTitulo();
+             
+             if(valor.equals(input)){
+                 livro = instancia.listaLivro.get(i);
+                 
+                 listaImpressão.add(livro);
+                 
+               }
                 
-                System.out.println("Insira o titulo: ");
-                String Titulo = sc.nextLine();
-                
-                for (int i = 0; i < instancia.listaLivro.size(); i++){
-                    String Valor = instancia.listaLivro.get(i).getTitulo();
-                    
-                    if(Valor.equals(Titulo)){
-                         System.out.println("Livro{ " + instancia.listaLivro.get(i).getID()
-                                              + ", " + instancia.listaLivro.get(i).getTitulo() 
-                                              + ", " + instancia.listaLivro.get(i).getGenero() 
-                                              + ", " + instancia.listaLivro.get(i).getAutor() 
-                                              + ", " + instancia.listaLivro.get(i).getEditora() 
-                                              + ", " + instancia.listaLivro.get(i).getAno() + " }");
-                         controle = true;
-                      }
-                  }
-                if (controle == false){
-                    System.out.println("Nenhum livro encontrado");
-                }
-                
-                break;
-                
+             }
+             
+             break;
+            
             case "genero":
-                
-                System.out.println("Insira o genero: ");
-                String Genero = sc.nextLine();
-                
-               for (int i = 0; i < instancia.listaLivro.size(); i++){
-                    String Valor = instancia.listaLivro.get(i).getGenero();
-                    
-                    if(Valor.equals(Genero)){
-                         System.out.println("Livro{ " + instancia.listaLivro.get(i).getID()
-                                              + ", " + instancia.listaLivro.get(i).getTitulo() 
-                                              + ", " + instancia.listaLivro.get(i).getGenero() 
-                                              + ", " + instancia.listaLivro.get(i).getAutor() 
-                                              + ", " + instancia.listaLivro.get(i).getEditora() 
-                                              + ", " + instancia.listaLivro.get(i).getAno() + " }");
-                         controle = true;
-                      }
-                  } 
-               
-               if (controle == false){
-                    System.out.println("Nenhum livro encontrado");
-                }
-               
-               break;
-               
+            for(int i = 0; i < instancia.listaLivro.size(); i++){
+             String valor = instancia.listaLivro.get(i).getGenero();
+             
+             if(valor.equals(input)){
+                 livro = instancia.listaLivro.get(i);
+                 
+                 listaImpressão.add(livro);
+                 
+               }
+             }
+            break;
+            
             case "autor":
                 
-                System.out.println("Insira o autor: ");
-                String Autor = sc.nextLine();
-                
-               for (int i = 0; i < instancia.listaLivro.size(); i++){
-                    String Valor = instancia.listaLivro.get(i).getAutor();
-                    
-                    if(Valor.equals(Autor)){
-                         System.out.println("Livro{ " + instancia.listaLivro.get(i).getID()
-                                              + ", " + instancia.listaLivro.get(i).getTitulo() 
-                                              + ", " + instancia.listaLivro.get(i).getGenero() 
-                                              + ", " + instancia.listaLivro.get(i).getAutor() 
-                                              + ", " + instancia.listaLivro.get(i).getEditora() 
-                                              + ", " + instancia.listaLivro.get(i).getAno() + " }");
-                         controle = true;
-                      }
-                  } 
-               
-               if (controle == false){
-                    System.out.println("Nenhum livro encontrado");
-                }   
-               
-               break;
-               
+             for(int i = 0; i < instancia.listaLivro.size(); i++){
+             String valor = instancia.listaLivro.get(i).getAutor();
+             
+             if(valor.equals(input)){
+                 livro = instancia.listaLivro.get(i);
+                 
+                 listaImpressão.add(livro);
+                 
+               }
+              }
+             break;
+             
             case "editora":
-                System.out.println("Insira o editor(a): ");
-                String Editora = sc.nextLine();
+             
+             for(int i = 0; i < instancia.listaLivro.size(); i++){
+             String valor = instancia.listaLivro.get(i).getEditora();
+             
+             if(valor.equals(input)){
+                 livro = instancia.listaLivro.get(i);
+                 
+                 listaImpressão.add(livro);
+                 
+               }
+               }
+             break;
+             
+            case "ano": 
                 
-               for (int i = 0; i < instancia.listaLivro.size(); i++){
-                    String Valor = instancia.listaLivro.get(i).getEditora();
-                    
-                    if(Valor.equals(Editora)){
-                         System.out.println("Livro{ " + instancia.listaLivro.get(i).getID()
-                                              + ", " + instancia.listaLivro.get(i).getTitulo() 
-                                              + ", " + instancia.listaLivro.get(i).getGenero() 
-                                              + ", " + instancia.listaLivro.get(i).getAutor() 
-                                              + ", " + instancia.listaLivro.get(i).getEditora() 
-                                              + ", " + instancia.listaLivro.get(i).getAno() + " }");
-                         controle = true;
-                      }
-                  } 
-               
-               if (controle == false){
-                    System.out.println("Nenhum livro encontrado");
-                }
-                break;
-               
-            case "ano":
-                
-                System.out.println("Insira o ano: ");
-                String AnoString = sc.nextLine();
-                
-                int Ano = Integer.parseInt(AnoString);
-                
-               for (int i = 0; i < instancia.listaLivro.size(); i++){
-                    int Valor = instancia.listaLivro.get(i).getAno();
-                    
-                    if(Valor == Ano){
-                         System.out.println("Livro{ " + instancia.listaLivro.get(i).getID()
-                                              + ", " + instancia.listaLivro.get(i).getTitulo() 
-                                              + ", " + instancia.listaLivro.get(i).getGenero() 
-                                              + ", " + instancia.listaLivro.get(i).getAutor() 
-                                              + ", " + instancia.listaLivro.get(i).getEditora() 
-                                              + ", " + instancia.listaLivro.get(i).getAno() + " }");
-                         controle = true;
-                      }
-                  } 
-               
-               if (controle == false){
-                    System.out.println("Nenhum livro encontrado");
-                }
-               
-               break;
-               
-            default:
-                System.out.println("Insira uma opção valida");
-    }       
-       
+             for(int i = 0; i < instancia.listaLivro.size(); i++){
+             int valor = instancia.listaLivro.get(i).getAno();
+             
+             int inputInt = Integer.parseInt(input);
+             
+             if(valor == inputInt){
+                 livro = instancia.listaLivro.get(i);
+                 
+                 listaImpressão.add(livro);
+                 
+               }
+             }
+             
+             break;
+             
+        }
+        
+        return listaImpressão;
         
     }
     
-    public void BuscarLivros(){
-        System.out.println("Como deseja buscar o livro: ");
-        System.out.println("1-Código (ID) | 2-Titulo"); 
-        String escolhaString = sc.nextLine();
+    
+    public ArrayList BuscarLivros(int Codigo){
         
-        int escolha = Integer.parseInt(escolhaString);
+        ArrayList listaImpressão = new ArrayList();
+        Model.Livros livro;
         
-        if(escolha == 1){
-            System.out.println("Digite o código do livro: ");
-            int Codigo = sc.nextInt();
+        for(int i = 0; i < instancia.listaLivro.size(); i++){
             
-            boolean controle = false;
+            int valorID = instancia.listaLivro.get(i).getID();
             
-            for (int i = 0; i < instancia.listaLivro.size(); i++){
-               
-                    int getID = instancia.listaLivro.get(i).getID();
-                    if (getID == Codigo){
-                        System.out.println("Livro{ " + instancia.listaLivro.get(i).getID()
-                                              + ", " + instancia.listaLivro.get(i).getTitulo() 
-                                              + ", " + instancia.listaLivro.get(i).getGenero() 
-                                              + ", " + instancia.listaLivro.get(i).getAutor() 
-                                              + ", " + instancia.listaLivro.get(i).getEditora() 
-                                              + ", " + instancia.listaLivro.get(i).getAno() + " }");
-                         controle = true;
-                    
-                }
-            }
-            if(controle == false){
-                        System.out.print("Livro não encontado!");
-                    }
-        }else if(escolha == 2){
-            System.out.println("Digite o titulo do livro: ");
-            String clear = sc.nextLine();
-            String Titulo = sc.nextLine();
-            boolean controle = false;
-            
-            for(int i = 0; i < instancia.listaLivro.size(); i++){
+            if(valorID == Codigo){
                 
-                String getTitulo = instancia.listaLivro.get(i).getTitulo();
-                
-                if(getTitulo.equals(Titulo)){
-                    System.out.println("Livro{ " + instancia.listaLivro.get(i).getID()
-                                              + ", " + instancia.listaLivro.get(i).getTitulo() 
-                                              + ", " + instancia.listaLivro.get(i).getGenero() 
-                                              + ", " + instancia.listaLivro.get(i).getAutor() 
-                                              + ", " + instancia.listaLivro.get(i).getEditora() 
-                                              + ", " + instancia.listaLivro.get(i).getAno() + " }");
-                    
-                    controle = true;
-                }
-            }
-            if (controle == false){
-                System.out.print("Livro não encontado!");
-            }
+                livro = instancia.listaLivro.get(i);
+                listaImpressão.add(livro);
+            
+            }  
         }
+        
+          return listaImpressão;
     }
+}
     
    
-}
+
 
 
